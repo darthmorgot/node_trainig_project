44ea4bf9cf4af199c7bd526d8a5406de188574c9
darthmorgot <dartmorgot@yandex.ru>
2021-03-09 23:15:11 +1000

    Закомментировал весь код, добавил вызов cli.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const args = process.argv.slice(2);
+// const args = process.argv.slice(2);
 
-switch (args[0]) {
-  case `--help`:
-    console.log(`This application does nothing. Accessible params:\n` +
-      `--help - prints this info;\n` +
-      `--version - prints application version;`);
-    break;
-  case `--version`:
-    console.log(`v1.0.0`);
-    break;
-  default:
-    console.log(`This application doesn\`t anything helpful. Just prints some info and version.`);
-    console.error(`To list possible options use \`--help\``);
-    process.exit(1);
-}
+// switch (args[0]) {
+//   case `--help`:
+//     console.log(`Available commands:\n` +
+//       `--author - shows author name;\n` +
+//       `--version - shows program version;\n` +
+//       `--help - prints this help;`);
+//     break;
+//   case `--version`:
+//     console.log(`v1.0.0`);
+//     break;
+//   default:
+//     console.log(`This application doesn\`t anything helpful. Just prints some info and version.`);
+//     console.error(`To list possible options use \`--help\``);
+//     process.exit(1);
+// }
+
+require('./src/cli.js');
