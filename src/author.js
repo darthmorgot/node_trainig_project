3ffd18afff375710ef642d499178733585ac5a5d
darthmorgot <dartmorgot@yandex.ru>
2021-03-13 20:21:06 +1000

    Файлы перемещены в папку cli

--- src/author.js
+++
@@ -1,9 +0,0 @@
-const packageInfo = require(`../package.json`);
-
-module.exports = {
-  name: `author`,
-  description: `Shows author name`,
-  execute() {
-    console.log(packageInfo.author);
-  }
-};
