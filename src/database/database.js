bbf83f7312ae1d91a3869a040edf890adbd9042d
darthmorgot <dartmorgot@yandex.ru>
2021-03-27 20:25:08 +1000

    Подключил logger из библиотеки winston, исправил код в связи с обращением к переменной окружения

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,11 @@
 const {MongoClient} = require(`mongodb`);
+const logger = require('../logger.js');
 
-const url = `mongodb://localhost:27017`;
+const url = process.env.MONGO_URL || `mongodb://localhost:27017`;
 
 module.exports = MongoClient.connect(url, {useUnifiedTopology: true})
   .then((client) => client.db(`code-and-magick`))
   .catch((e) => {
-    console.log(`Failed to connect to MongoDB`, e);
+    logger.info(`Failed to connect to MongoDB`, e);
     process.exit(1);
   });
